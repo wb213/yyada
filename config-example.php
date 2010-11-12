d7f12d1e78e2d383517819f63beab731dede5929
barry213 <barry213@6fd97ab8-3246-d4b8-17d4-d8a37aef0fc2>
2010-11-12 15:48:48 +0000

    rename config.php to config-example.php
    git-svn-id: https://yyada.googlecode.com/svn/trunk@38 6fd97ab8-3246-d4b8-17d4-d8a37aef0fc2

--- config-example.php
+++ config-example.php
@@ -1,9 +1,9 @@
 <?php
 
-define('BASE_URL', 'https://your.own.domain');
+define('BASE_URL', 'http://your.own.domain');
 define('SECRET_KEY', 'Sample Key - Change!');
 
-define('OAUTH_PROXY', 'https://your.own.oauth.proxy');
+define('OAUTH_PROXY', 'http://your.own.oauth.proxy');
 
 define('CONSUMER_KEY', 'change to your own consumer key');
 define('CONSUMER_SECRET', 'change to your own consumer secret');
