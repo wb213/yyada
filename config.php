e2d54cd20c4018131092b4132c8be79b67bc7c6a
barry213 <barry213@6fd97ab8-3246-d4b8-17d4-d8a37aef0fc2>
2010-11-12 15:00:27 +0000

    add config.php
    git-svn-id: https://yyada.googlecode.com/svn/trunk@37 6fd97ab8-3246-d4b8-17d4-d8a37aef0fc2

---
+++ config.php
@@ -0,0 +1,13 @@
+<?php
+
+define('BASE_URL', 'https://your.own.domain');
+define('SECRET_KEY', 'Sample Key - Change!');
+
+define('OAUTH_PROXY', 'https://your.own.oauth.proxy');
+
+define('CONSUMER_KEY', 'change to your own consumer key');
+define('CONSUMER_SECRET', 'change to your own consumer secret');
+
+define('ENABLE_INVITE', 'true'); # true for enable.
+
+?>
