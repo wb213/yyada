5c5dd031b937714e3b3ce23ca545e01fe3815ac6
Barry Wang <barry213@gmail.com>
2010-11-28 14:12:34 +0800

    bug fix for timeline

--- control/timeline.php
+++ control/timeline.php
@@ -1,46 +1,11 @@
 <?php
-require_once('config.php');
-require_once('core/twitteroauth.php');
-require_once('core/settings.php');
-require_once('core/theme.php');
-require_once('util/cookie.php');
-require_once('util/factory.php');
-require_once('util/url.php');
-require_once('util/tweet.php');
-require_once('util/tag.php');
 
-function update() {
-  global $conn;
-  $post_data = array("status" => $_POST['status']);
-  if (!empty($_POST['in_reply_to_id']))
-    $post_data = array_merge($post_data, array("in_reply_to_status_id" => $_POST['in_reply_to_id']));
-  if (!empty($_POST['location'])) {
-    list($lat, $long) = explode(',', $_POST['location']);
-    $post_data = array_merge($post_data, array("lat" => $lat, "long" => "$long"));
-  }
-  $conn->post('statuses/update', $post_data);
-}
-
-function get_reply_thread($tweet_id) {
-  global $conn;
-  $ret = array();
-  do {
-    $t = $conn->get('statuses/show/'.$tweet_id);
-    array_push($ret, $t);
-    $tweet_id = $t->in_reply_to_status_id_str;
-  } while (!empty($tweet_id));
-  return $ret;
-}
-
-function get_reply_users($tweet_id) {
-  global $conn;
-  $t = $conn->get('statuses/show/'.$tweet_id);
-  $users = get_mentioned_users('@'.$t->user->screen_name.' '.$t->text);
-  return implode($users, ' ').' ';
-}
+require_once('core/tweets.php');
 
 function load_timeline() {
-	global $access_token, $content, $conn, $action, $target;
+	global $access_token, $content, $conn, $action, $target, $display;
+
+	$display = true;
 
 	if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 	  switch ($action) {
@@ -73,6 +38,7 @@ function load_timeline() {
 		}
 		
 	  	$content = array_merge($content, array('tweets' => $tweets));
+	}
 }
 
 ?>
