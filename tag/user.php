3183469c17bc6017c6373ee2a20f2c7497705933
Googol Lee <googollee@gmail.com>
2010-12-02 00:27:03 +0800

    add avatar settings in user pag.

--- tag/user.php
+++ tag/user.php
@@ -1,11 +1,14 @@
 <?php
 
 function user_info_html() {
-  global $content;
+  global $settings, $content;
   if (!isset($content['tweets'])) return;
  
   $tweet = $content['tweets'][0];
-  echo "<img src='".$tweet->user->profile_image_url."' alt='".$tweet->user->name."' />";
+  if ($settings->show_avatar)
+    echo "<img src='".$tweet->user->profile_image_url."' alt='".$tweet->user->name."' />";
+  else
+    echo "<a href='".$tweet->user->profile_image_url."' alt='".$tweet->user->name."'>Avatar</a>";
   echo "<a class='name' href='".join_path(BASE_URL, "user/show", $tweet->user->screen_name)."'>".$tweet->user->screen_name."</a>"."(".$tweet->user->name.")<br/>";
   echo "Bio: ". $tweet->user->description."<br/>";
   echo "Link: <a target='_blank' href='".$tweet->user->url."'>".$tweet->user->url."</a><br/>";
