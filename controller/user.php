6a984dd89534802eb613a9bcd5a96a6d1e68168a
Barry Wang <barry213@gmail.com>
2010-12-02 15:55:05 +0800

    try implement followers and friends page

--- controller/user.php
+++ controller/user.php
@@ -9,6 +9,26 @@ function show($user) {
   $theme->include_html('user');
 }
 
+function followers($user) {
+  global $content, $conn, $theme, $access_token;
+
+  if (empty($user)) $user = $access_token['screen_name'];
+
+  $user_list = $conn->get('statuses/followers', array("screen_name" => $user));
+  $content['user_list'] = $user_list;
+  $theme->include_html('user_list');
+}
+
+function friends($user) {
+  global $content, $conn, $theme, $access_token;
+
+  if (empty($user)) $user = $access_token['screen_name'];
+
+  $user_list = $conn->get('statuses/friends', array("screen_name" => $user));
+  $content['user_list'] = $friend_list;
+  $theme->include_html('user_list');
+}
+
 function default_behavior() {
   global $access_token;
 
