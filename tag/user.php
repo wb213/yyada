3bcfeca86b5d7445100b8ceabea61674c0a3aac3
Barry Wang <barry213@gmail.com>
2010-12-03 13:44:37 +0800

    adjust user page

--- tag/user.php
+++ tag/user.php
@@ -4,16 +4,33 @@ function user_info_html() {
   global $settings, $content;
   if (!isset($content['tweets'])) return;
  
-  $tweet = $content['tweets'][0];
+  $user = $content['tweets'][0]->user;
+
+  $name = $user->name;
+  $screen_name = $user->screen_name;
+  $img_url = $user->profile_image_url;
+  $desc = $user->description;
+  $url = $user->url;
+  $loc = $user->location;
+  $join = format_time(strtotime($user->created_at), $user->utc_offset);
+  $tweets = $user->statuses_count . " tweets";
+  $friends = $user->friends_count . " friends";
+  $followers = $user->followers_count . " followers";
+  $favs = $user->favourites_count . " favs";
+  $protected = $user->protected;
+
   if ($settings->show_avatar)
-    echo "<img src='".$tweet->user->profile_image_url."' alt='".$tweet->user->name."' />";
+    echo "<img src='".$img_url."' alt='".$name."' />";
   else
-    echo "<a href='".$tweet->user->profile_image_url."' alt='".$tweet->user->name."'>Avatar</a>";
-  echo "<a class='name' href='".make_path("user/show/".$tweet->user->screen_name)."'>".$tweet->user->screen_name."</a>"."(".$tweet->user->name.")<br/>";
-  echo "Bio: ". $tweet->user->description."<br/>";
-  echo "Link: <a target='_blank' href='".$tweet->user->url."'>".$tweet->user->url."</a><br/>";
-  echo "Location: ". $tweet->user->location."<br/>";
-  echo "Joined: ". $tweet->user->created_at."<br/>";
+    echo "<a href='".$img_url."' alt='".$name."'>Avatar</a>";
+  if ($protected) echo "PROTECTED USER";
+  echo "<br />";
+  echo "<a class='name' href='".make_path("user/show/".$screen_name)."'>".$screen_name."</a>"."(".$name.")";
+  echo "Bio: ". $desc."<br/>";
+  echo "Link: <a target='_blank' href='".$url."'>".$url."</a><br/>";
+  echo "Location: ". $loc."<br/>";
+  echo "Joined: ". $join ."<br/>";
+  echo "Info: " . $tweets . ", " . $friends . ", " . $followers . ", " . $favs;
 }
 
 function has_user_list() {
@@ -35,11 +52,11 @@ function list_user_html() {
   $screen_name = $users->screen_name;
   $img_url = $users->profile_image_url;
   $desc = $users->description;
-  $tweets = $users->statuses_count . "tweets";
-  $friends = $users->friends_count . "friends";
-  $followers = $users->followers_count . "followers";
-  $favs = $users->favourites_count . "favs";
-  $lists = $users->listed_count . "lists";
+  $tweets = $users->statuses_count . " tweets";
+  $friends = $users->friends_count . " friends";
+  $followers = $users->followers_count . " followers";
+  $favs = $users->favourites_count . " favs";
+  $lists = $users->listed_count . " lists";
 
   if ($settings->show_avatar) {
         echo "<img class='avatar' src='".$img_url."' alt='".$name."' />";
