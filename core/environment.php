6139dcb88ee1e3fee553b1d0b90e404b0318c146
Googol Lee <googollee@gmail.com>
2010-12-05 14:49:31 +0800

    simple the code

--- core/environment.php
+++ core/environment.php
@@ -36,17 +36,15 @@ function dispatch_url() {
   }
   include 'controller/' . $controller;
 
-  $action = 'default_behavior';
+  $action = 'default';
   if (isset($uri[1]) && !empty($uri[1]))
     $action = $uri[1];
-  if (!in_array($action, array_keys($controller_router)))
-    $action = 'default';
 
   $args = '';
   if (isset($uri[2]) && !empty($uri[2]))
     $args = $uri[2];
 
-  $controller_router[$action]($args);
+  array_get($controller_router, $action, 'default')($args);
 }
 
 function init_environment() {
