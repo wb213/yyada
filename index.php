8f7dd2ee7afa050f5748dc0bea77a35f3d7b0700
Googol Lee <googollee@gmail.com>
2010-12-05 12:54:43 +0800

    use exception when init

--- index.php
+++ index.php
@@ -8,7 +8,10 @@ require_once('tag/include.php');
 
 try {
   init_environment();
+} catch (NoCookie $e) {
+  error_log($e->getMessage());
 } catch (Exception $e) {
+  error_log($e->getMessage());
   $_SERVER['REQUEST_URI'] = make_path('/login/clear');
 }
 
