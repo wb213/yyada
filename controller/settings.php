c70a6406041bedf196da9d484c43458a5fac74bb
Googol Lee <googollee@gmail.com>
2010-12-06 15:59:16 +0800

    fix reverse

--- controller/settings.php
+++ controller/settings.php
@@ -14,7 +14,6 @@ function show() {
   if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     if (isset($_POST['theme'])) $settings->theme = $_POST['theme'];
     $settings->show_avatar = isset($_POST['avatar']);
-    $settings->is_reverse_thread = isset($_POST['reverse']);
     $settings->show_img = isset($_POST['img']);
     if (isset($_POST['rt_format'])) $settings->rt_format = $_POST['rt_format'];
 
