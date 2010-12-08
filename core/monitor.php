7ea75684454f2a75923e832f4007f42fed36000f
Googol Lee <googollee@gmail.com>
2010-12-09 00:09:10 +0800

    change monitor

--- core/monitor.php
+++ core/monitor.php
@@ -1,54 +1,48 @@
 <?php
 
 require_once('core/cookie.php');
+require_once('util/url.php');
 
 class Monitor {
 
   private $urls = array(
-    'statuses/mentions',
-    'direct_messages',
+    'mention' => array('twitter' => 'statuses/mentions', 'yyada' => 'tweet/mention'),
+    'direct' => array('twitter' => 'direct_messages', 'yyada' => 'direct'),
   );
 
   private $urls_key = 'monitor';
   private $time_key = 'check';
   private $interval = 300; // 5 * 60 seconds
 
-  public function __construct($s = null) {
-    if (isset($s) && !empty($s)) {
-      $args = explode('|', $s);
-      foreach ($args as $url) {
-        $this->add($url);
-      }
-    }
-  }
-
-  public function find($url) {
+  public function find($name) {
+    $keys = array_keys($this->urls);
     for ($i=0; $i<count($this->urls); $i++)
-      if ($url == $this->urls["$i"])
+      if ($keys["$i"] == $name)
         return $i;
     return -1;
   }
 
   public function get($index) {
-    return $this->urls["$i"];
+    $keys = array_keys($this->urls);
+    return $this->urls[$keys["$i"]];
   }
 
-  public function add($url) {
-    $index = $this->find($url);
+  public function add($name, $twitter_url, $yyada_url) {
+    $index = $this->find($name);
 
     if ($index == -1)
-      array_push($this->addresses, $url);
+      $this->urls[$name] = array('twitter' => $twitter_url, 'yyada' => $yyada_url);
   }
 
-  public function is_new($url) {
-    return isset($_SESSION[$url]);
+  public function is_new($name) {
+    return isset($_SESSION[$name]);
   }
 
-  public function remove($url) {
-    $index = $this->find($url);
+  public function remove($name) {
+    $index = $this->find($name);
 
     if ($index != -1)
-      unset($this->addresses[$index]);
+      unset($this->urls[$name]);
   }
 
   public function check_new() {
@@ -63,11 +57,16 @@ class Monitor {
     if (($now / $this->interval) <= ($last / $this->interval))
       return;
 
-    foreach ($this->urls as $url) {
-      if ($this->is_new($url))
+    foreach ($this->urls as $name => $url) {
+      if ($_SERVER['REQUEST_URI'] == make_path($url['yyada'])) {
+        unset($_SESSION[$name]);
+        continue;
+      }
+
+      if ($this->is_new($name))
         continue;
 
-      $tweets = $conn->get($url);
+      $tweets = $conn->get($url['twitter']);
       $time = strtotime($tweets[0]->created_at);
       if ($time > $last)
         $_SESSION[$url] = true;
