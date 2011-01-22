3bfde5a680d5168e1ca8eecbc78617d98082be36
Googol Lee <googollee@gmail.com>
2011-01-22 23:10:38 +0800

    Add list add/remove

--- tag/list.php
+++ tag/list.php
@@ -27,8 +27,10 @@ function list_lists_item_html() {
 
   echo "<div class='toolbar'>";
   echo "<a class='screen_name' href='".make_path(join_path('list/show', $list->uri))."'>".$list->full_name."</a>";
-  if ($list->user->screen_name == $access_token['screen_name'])
+  if ($list->user->screen_name == $access_token['screen_name']) {
+    echo "<a class='edit' href='".make_path(join_path('list/remove', $list->uri))."'>Remove</a>";
     echo "<a class='edit' href='".make_path(join_path('list/edit', $list->uri))."'>Edit</a>";
+  }
   echo "<a class='member' href='".make_path(join_path('list/member', $list->uri))."'>Members(".$list->member_count.")</a>";
   echo "<a class='suber' href='".make_path(join_path('list/suber', $list->uri))."'>Subers(".$list->subscriber_count.")</a>";
   $monitor_name = 'list'.$list->uri;
@@ -55,6 +57,37 @@ function list_edit_html() {
     <input class='input' type='checkbox' name='mode'".($list->mode=='private'?" checked='checked'":"")." /><span class='title'> Is private?</span>
   </div>
   <input type='submit' value='Submit' />
+</form>";
+}
+
+function list_new_html() {
+  echo "
+<form class='update' method='post' action='".make_path('list/add')."'>
+  <div class='name'>
+    <span class='title'>Name: </span><textarea class='input' name='name' rows='1'></textarea>
+  </div>
+  <div class='desciption'>
+    <span class=title'>Description: </span><textarea class='input' name='description' rows='1'></textarea>
+  <div class='mode'>
+    <input class='input' type='checkbox' name='mode' /><span class='title'> Is private?</span>
+  </div>
+  <input type='submit' value='Submit' />
+</form>";
+}
+
+function list_new() {
+  echo "
+<div classs='add'>
+  <a href='".make_path('list/add')."'>Create new list</a>
+</div>";
+}
+
+function list_remove_html() {
+  global $content;
+
+  echo "
+<form class='update' method='post' action='".make_path('list/remove/'.$content['list'])."'>
+  <input type='submit' value='Yes please' />
 </form>";
 }
 
