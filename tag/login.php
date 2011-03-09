ea1c469edad0d4da896a85f214e3959f63d3395d
Googol Lee <googollee@gmail.com>
2011-03-09 09:07:26 +0800

    add twitter official web oauth login

--- tag/login.php
+++ tag/login.php
@@ -6,7 +6,8 @@ function login_html($echo = true) {
   $ret =
 '<div>
   <h2>Welcome to yyada, Yet Yyada Another DAbr.</h2>
-  <a href="'.make_path("login/oauth").'"><img src="'.make_path('twitter_button.gif').'" alt="Sign in with Twitter/OAuth" width="165" height="28" /></a>
+  <a href="'.make_path("login/oauth").'">Login with oauth proxy</a><p>or</p>
+  <a href="https://api.twitter.com/oauth"><img src="'.make_path('twitter_button.gif').'" alt="Sign in with Twitter/OAuth" width="165" height="28" /></a>
 </div>';
   if ($echo) echo $ret;
   return $ret;
